b5a49cc320a615a4383228906b8fe1e070ef53c9
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-11-24 16:05:27 +0700

    fix(DarkMode): animation toggle and transition

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,43 +1,47 @@
-import React, {useState, useEffect} from "react";
-import {Moon, Sun} from "lucide-react";
+import React, { useState, useEffect } from "react";
+import { Moon, Sun } from "lucide-react";
 
 export function ModeToggle() {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-    useEffect(() => {
-        const isDark = document.documentElement.classList.contains("dark");
-        setIsDarkMode(isDark);
-    }, []);
+  useEffect(() => {
+    const isDark = document.documentElement.classList.contains("dark");
+    setIsDarkMode(isDark);
+  }, []);
 
-    useEffect(() => {
-        document.documentElement.classList[isDarkMode ? "add" : "remove"]("dark");
-    }, [isDarkMode]);
+  useEffect(() => {
+    document.documentElement.classList[isDarkMode ? "add" : "remove"]("dark");
+  }, [isDarkMode]);
 
-    const toggleTheme = () => {
-        document.documentElement.classList.add('theme-transitioning');
-        setIsDarkMode((prev) => !prev)
+  const toggleTheme = () => {
+    document.documentElement.classList.add("theme-transitioning");
+    setIsDarkMode((prev) => !prev);
 
-        setTimeout(() => {
-            document.documentElement.classList.remove('theme-transitioning');
-        }, 500);
-    };
+    setTimeout(() => {
+      document.documentElement.classList.remove("theme-transitioning");
+    }, 500);
+  };
 
-    return (
-        <>
-            <button
-                onClick={toggleTheme}
-                className={`relative flex h-8 w-16 items-center rounded-full transition-colors ${
-                    isDarkMode ? "bg-gray-800" : "border border-gray-800"
-                }`}
-            >
-      <span
-          className={`absolute flex h-6 w-6 items-center justify-center rounded-full bg-orange-500 text-white transition-transform ${
-              isDarkMode ? "translate-x-0" : "translate-x-8"
-          }`}
+  return (
+    <>
+      <button
+        onClick={toggleTheme}
+        className={`relative flex h-8 w-14 items-center rounded-full transition-colors ${
+          isDarkMode ? "border border-white" : "border border-gray-800"
+        }`}
       >
-        {isDarkMode ? <Moon className="h-4 w-4"/> : <Sun className="h-4 w-4"/>}
-      </span>
-            </button>
-        </>
-    );
+        <span
+          className={`absolute flex h-6 w-6 items-center justify-center rounded-full bg-orange-500 text-white transition-transform ${
+            isDarkMode ? "translate-x-1" : "translate-x-6"
+          }`}
+        >
+          {isDarkMode ? (
+            <Moon className="h-4 w-4" />
+          ) : (
+            <Sun className="h-4 w-4" />
+          )}
+        </span>
+      </button>
+    </>
+  );
 }
