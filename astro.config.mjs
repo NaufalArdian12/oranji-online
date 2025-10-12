07fb0d42c045c4c7a9c3837b15be8fec7d41dcd7
SyahrulBhudiF <syahrul4w@gmail.com>
2025-10-12 18:02:21 +0700

    chore: update depedency and config linting with biomejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind({applyBaseStyles: false})],
-    vite: {
-        ssr: {
-            noExternal: ['react-icons']
-        },
-        build: {
-            sourcemap: true
-        },
-        optimizeDeps: {
-            include: ['react', 'react-dom', 'framer-motion']
-        }
-    }
+	integrations: [react()],
+	vite: {
+		plugins: [tailwindcss()],
+		ssr: {
+			noExternal: ["react-icons"],
+		},
+		build: {
+			sourcemap: true,
+		},
+		optimizeDeps: {
+			include: ["react", "react-dom", "framer-motion"],
+		},
+	},
 });
--- src/lib/schema/types.ts
+++ src/lib/schema/types.ts
@@ -1,10 +1,10 @@
-import React from "react";
+import type React from "react";
 
 type NavItem = {
-    id: string;
-    label: string;
-    path: string;
-    icon: React.ReactNode;
+	id: string;
+	label: string;
+	path: string;
+	icon: React.ReactNode;
 };
 
-export type {NavItem};
+export type { NavItem };
